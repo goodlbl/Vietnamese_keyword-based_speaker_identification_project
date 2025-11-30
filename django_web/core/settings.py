74517be498cee6b7939f1c31c3751d4c3fcbe78f
Dylan12211 <149658291+Dylan12211@users.noreply.github.com>
2025-11-30 22:54:33 +0700

    cleanup: remove pycache, pyc, db.sqlite3 from git

--- django_web/core/settings.py
+++ django_web/core/settings.py
@@ -15,8 +15,8 @@ import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
